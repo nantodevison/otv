d4c01326f4a03fe586d3a727f525a783a42b0184
Martin_boulot <martin.schoreisz@cerema.fr>
2023-10-04 15:50:03 +0200

    RTest/GeolocInverseIGN/enCours1

--- otv/Transfert_Donnees/FME/RTest/GeolocInverseIGN/CreerTableResultatBdd.sql
+++ otv/Transfert_Donnees/FME/RTest/GeolocInverseIGN/CreerTableResultatBdd.sql
@@ -1,3 +1,37 @@
 -- New script in postgres.
 -- Date: 4 oct. 2023
 -- Time: 14:07:24
+
+/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
+ * Script de création de la table acceuillant les résultats de tests de RTest
+*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
+
+/* ===============
+ * Schema et table
+ ================= */
+
+-- Si beson créé le schéma
+CREATE SCHEMA rtest ; 
+
+--création du type énuméré de gestion des natures de reverse geocoding
+CREATE TYPE rtest.reverse_geoloc_type_enum AS ENUM ('StreetAddress', 'CadastralParcel', 'PointOfInterest', 'Administratif', 'PasDeGeomSource') ;
+--creation de la table
+CREATE TABLE rtest.geoloc_inverse_ign 
+ (id serial PRIMARY key,
+  id_objet_test integer NOT NULL,
+  "GeolocInverse.Numero" varchar,
+  "GeolocInverse.Rue" varchar,
+  "GeolocInverse.Commune" varchar,
+  "GeolocInverse.Departement" varchar,
+  "GeolocInverse.CodeCommuneInsee" varchar,
+  "GeolocInverse.CodePostal" varchar,
+  "GeolocInverse.DistanceObjet" varchar,
+  "GeolocInverse.x_wgs84" real,
+  "GeolocInverse.y_wgs84" real,
+  "GeolocInverse.ParcelleComplete" varchar,
+  "GeolocInverse.Feuille" varchar,
+  "GeolocInverse.Section" varchar,
+  "GeolocInverse.CommuneAbsorbee" varchar,
+  "GeolocInverse.Arrondissement" varchar,
+  "GeolocInverse.type" varchar,
+  "GeolocInverse.typeGeoloc" rtest.reverse_geoloc_type_enum) ;
